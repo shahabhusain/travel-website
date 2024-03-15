cd0421c68a20aa4e96f888ca93d5a41454fdc43a
shahab khan <hussainmuhammad098123123@gmail.com>
2024-03-15 02:33:18 -0700

    commit

--- src/Components/About/PopularTour.jsx
+++ src/Components/About/PopularTour.jsx
@@ -13,14 +13,14 @@ const PopularTour = () => {
     <h2 className=' text-[#DF6951] font-bold'>TREND</h2>
     <h1 className=' xl:text-[53px] md:text-[33px] text-[25px] font-bold'>Our Popular Tour Plans</h1>
     <p>Et labore harum non nobis ipsum eum molestias mollitia et corporis praesentium a laudantium. Et labore harum non nobis ipsum eum molestias mollitia et corporis praesentium a laudantium.</p>
-  <div className=' flex items-center gap-12'>
-    <div className=' xl:w-[150px] md:w-[100px] w-[100px]'> 
+  <div className=' flex items-center md:gap-12 gap-6'>
+    <div className=' xl:w-[150px] md:w-[100px] w-[70px]'> 
     <CircularProgressbar value={percentage} text={`${percentage}%`} />
     </div>
-    <div className=' xl:w-[150px] md:w-[100px] w-[100px]'> 
+    <div className=' xl:w-[150px] md:w-[100px] w-[70px]'> 
     <CircularProgressbar value={percentage1} text={`${percentage1}%`} />
     </div>
-    <div className=' xl:w-[150px] md:w-[100px] w-[100px]'> 
+    <div className=' xl:w-[150px] md:w-[100px] w-[70px]'> 
     <CircularProgressbar value={percentage2} text={`${percentage2}%`} />
     </div>
     </div>
--- src/Components/About/Wonder.jsx
+++ src/Components/About/Wonder.jsx
@@ -4,7 +4,7 @@ const Wonder = () => {
   return (
     <div className=' back7 mt-24'>
       <div className=' flex items-center justify-center'>
-        <h1 className=' title font-[800] text-[77px] text-white mt-24'>Wanderlust</h1>
+        <h1 className=' title font-[800] md:text-[77px] text-[54px] text-white md:mt-24 mt-32'>Wanderlust</h1>
       </div>
     </div>
   )
